82cf7c7d988210b36e29f5bd080b945f35190650
flyingwonton <66998126+flyingwonton@users.noreply.github.com>
2022-10-07 11:17:14 +0200

    removed useless functions

--- main.py
+++ main.py
@@ -7,56 +7,55 @@ import streamlit as st
 
 
 
-def main():
+def main(uploaded_pics):
     counter = 0
-    exif_dict = {}
     gps_info = {}
-    ext = get_ext()
-    path = getpath()
-    filelist = glob.glob(path + "*" + ext)
-    try:
-        writer = pd.ExcelWriter('export.xlsx', engine='xlsxwriter')
-    except PermissionError:
-        print("Please close export.xlsx and try again.\nExiting..")
-        sys.exit()
-    for element in filelist:
-        name = getname(counter, ext, path)
-        worksheetname = name  #the name must not exceed the 31 chars
-        im = Image.open(element)
-        im_exif = getexifmethod(ext, im)
-        counter += 1
+    exif_dict = {}
+    dataframes = {}
+    if uploaded_pics is not None:  # se effettivamente c'è qualcosa
+        for element in uploaded_pics:
+            ext = get_ext(element.name)
+            im = Image.open(element)
+            im_exif = getexifmethod(ext, im)
+            counter += 1
+            # https://stackoverflow.com/questions/69694259/create-dataframe-variables-inside-for-loop-group-dataframes
 
-        try:
             if im_exif is None:
-                print("exif data not available")
+                st.write(f"exif data not available for {element.name[:40]}")
             else:
-                for key, val in im_exif.items():
-                    if key in ExifTags.TAGS:
-                        exif_dict[ExifTags.TAGS[key]] = val
-                for key, val in exif_dict['GPSInfo'].items():
-                    if key in ExifTags.GPSTAGS:
-                        gps_info[ExifTags.GPSTAGS[key]] = val
-                exif_dict.update(gps_info)
-                exif_dict.pop("GPSInfo") #delete double GPSInfo tag
+                try:
+                    for key, val in im_exif.items():
+                        if key in ExifTags.TAGS:
+                            exif_dict[ExifTags.TAGS[key]] = val
+                    try:  # Check if there is GPS info available
+                        for key, val in exif_dict['GPSInfo'].items():
+                            if key in ExifTags.GPSTAGS:
+                                gps_info[ExifTags.GPSTAGS[key]] = val
+                                st.write(str(val))
+                        exif_dict.update(gps_info)
+                        exif_dict.pop("GPSInfo")  # delete double GPSInfo tag
+                    except KeyError:
+                        st.write(f"GPS Info not found for {element.name[:40]}")
+                        pass  # crack on
+                    df = pd.DataFrame(list(exif_dict.items()), columns=['Tags', 'Values'])
+                    dataframes[
+                        f'df{counter}'] = df  # https://stackoverflow.com/questions/69694259/create-dataframe-variables-inside-for-loop-group-dataframes
+                    csv = convert_df(dataframes[f'df{counter}'])
+                    st.download_button(
+                        label=f"Download {element.name[:30]} data as CSV",  # [:30] prende i primi trenta caratteri
+                        data=csv,
+                        file_name=f'df{element.name[:30]}.csv',
+                        mime='text/csv',
+                    )
 
-        except KeyError:
-            print("Some exif tags might have been not found.")
+                    df_as_string = df.astype(
+                        str)  # converte il df to string per evitare l'errore "Conversion failed for column Values with type object'"
+                    st.write(df_as_string)
+                except:
+                    st.write("Ni dobro:", str(sys.exc_info()[0]), "occurred.")
+                    st.write(str(sys.exc_info()[1:]))
+        st.write(dataframes)
 
-        except:
-            print("Ni dobro:", sys.exc_info()[0], "occurred.")
-        df = pd.DataFrame(list(exif_dict.items()), columns=['Tags', 'Values'])
-        df.to_excel(writer, index=False, sheet_name=worksheetname)
-    writer.save()
-    print("Exif tags exported successfully.")
-
-def getname(count, ext, path):
-    names = [os.path.basename(x) for x in glob.glob(path + "*" + ext)] #get the names from the file list
-    n0 = str(names[count])
-    if len(n0) > 31:
-        n = n0[:31]
-    else:
-        n = n0
-    return n
 
 def getexifmethod(ext, im): #temporary workaround
     if ext == ".tif" or ext == ".tiff":
@@ -64,24 +63,6 @@ def getexifmethod(ext, im): #temporary workaround
     else:
         im_exif = im._getexif()
     return im_exif
-
-def getpath():
-    try:
-        path = input(str("Provide the path where you stored your photos: "))
-        if path[-1] == "\\":
-            pass
-        elif path[-1] != "\\":
-            path = path + "\\"
-
-    except:
-        print("Ni dobro:", sys.exc_info()[0], "occurred.")
-
-    if os.path.isdir(path):
-        print(f"Path provided: {path}")
-    else:
-        print(f" '{path}' is not a valid path. The default path used is where this script is located.")
-        path = os.path.abspath(os.path.dirname(__file__)) + "\\"  # takes the directory where the script is located
-    return path
 
 def get_ext(filename):
     extensions = ["jpg", "jpeg", "jpe", "jif", "jfif", "jfi", "tif", "tiff", "riff", "jpg", "jpeg", "jpe", "jif", "jfif", "jfi", "tif", "tiff", "riff"] #compatible extensions, it's not elegant but it works
@@ -101,58 +82,12 @@ if __name__ == '__main__':
     st.header("Bulk exif to excel web app")
     filetypes = ["jpg", "jpeg", "jpe", "jif", "jfif", "jfi", "tif", "tiff", "riff", "jpg", "jpeg", "jpe", "jif", "jfif",
                  "jfi", "tif", "tiff", "riff"]
-    uploaded_pics = st.file_uploader("Carica le foto da cui esportare gli exif", type=filetypes,
+    uploaded_pics = st.sidebar.file_uploader("Carica le foto da cui esportare gli exif", type=filetypes,
                                      accept_multiple_files=True)
-    counter = 0
-    gps_info = {}
-    exif_dict = {}
-    dataframes = {}
-    if uploaded_pics is not None:  # se effettivamente c'è qualcosa
-        for element in uploaded_pics:
-            ext = get_ext(element.name)
-            im = Image.open(element)
-            im_exif = getexifmethod(ext, im)
-            counter += 1
-            #https://stackoverflow.com/questions/69694259/create-dataframe-variables-inside-for-loop-group-dataframes
 
-            if im_exif is None:
-                st.write("exif data not available")
-            else:
-                try:
-                    for key, val in im_exif.items():
-                        if key in ExifTags.TAGS:
-                            exif_dict[ExifTags.TAGS[key]] = val
-                    try: #Check if there is GPS info available
-                        for key, val in exif_dict['GPSInfo'].items():
-                            if key in ExifTags.GPSTAGS:
-                                gps_info[ExifTags.GPSTAGS[key]] = val
-                                st.write(str(val))
-                        exif_dict.update(gps_info)
-                        exif_dict.pop("GPSInfo")  # delete double GPSInfo tag
-                    except KeyError:
-                        st.write("GPS Info not found")
-                        pass # crack on
-                    df = pd.DataFrame(list(exif_dict.items()), columns=['Tags', 'Values'])
-                    dataframes[f'df{counter}'] = df #https://stackoverflow.com/questions/69694259/create-dataframe-variables-inside-for-loop-group-dataframes
-                    csv = convert_df(dataframes[f'df{counter}'])
-                    st.download_button(
-                        label=f"Download {element.name[:30]} data as CSV", # [:30] prende i primi trenta caratteri
-                        data=csv,
-                        file_name=f'df{element.name[:30]}.csv',
-                        mime='text/csv',
-                    )
-
-                    df_as_string = df.astype(
-                        str)  # converte il df to string per evitare l'errore "Conversion failed for column Values with type object'"
-                    st.write(df_as_string)
-                except:
-                    st.write("Ni dobro:", str(sys.exc_info()[0]), "occurred.")
-                    st.write(str(sys.exc_info()[1:]))
-        st.write(dataframes)
         #st.write(dataframes[f'df{counter}'].astype(str))
 
      #https://docs.streamlit.io/library/api-reference/widgets/st.file_uploader
 
 
-    pass
-    #main()
+    main(uploaded_pics)
